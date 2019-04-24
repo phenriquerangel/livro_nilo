2970f222fa4f7fd092495f6faafbabb016857df5
root <root@DESKTOP-GGPH27C.localdomain>
2019-04-24 02:36:08 +0000

    exercicie 3.15

---
+++ cap3/ex3-15.py
@@ -0,0 +1,9 @@
+cigarrets = int(input("Cigarrets: "))
+year = int(input("Years: "))
+
+
+cig_per_day = (year*365)*cigarrets
+
+time_left=((cig_per_day/60)/24)*10
+
+print("Time over: %0.1f days" % time_left)
